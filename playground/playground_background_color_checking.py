231f3af79aeb191ac070840888708d4afcbac53c
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2024-01-05 23:54:36 +0100

    add playground for daemonizing a method

--- playground/playground_background_color_checking.py
+++ playground/playground_background_color_checking.py
@@ -1,6 +1,9 @@
 """How do I make the `check_color` method run in the background?"""
 
-from time import sleep
+import time
+import threading
+
+from playground_victory_detection import check_victory
 
 class GenshinEnv:
     def run_left(self):
@@ -10,14 +13,35 @@ class GenshinEnv:
     def check_color():
         while True:
             print('checking color')
-            sleep(0.2)
+            time.sleep(0.2)
 
+class TestThreading:
+    def __init__(self, interval=1):
+        self.interval = interval
+
+        thread = threading.Thread(target=self.run, args=())
+        thread.daemon = True
+        thread.start()
+
+    def run(self):
+        while True:
+            # More statements comes here
+            print(f'{time.time()}: Running in the background')
+
+            time.sleep(self.interval)
 
 if __name__ == '__main__':
-    env = GenshinEnv()
-    env.check_color()
-    env.run_left()
-    env.run_left()
-    env.run_left()
-    env.run_left()
-    env.run_left()
+    tr = TestThreading()
+    time.sleep(1)
+    print(f'{time.time()}: First output')
+
+    time.sleep(2)
+    print(f'{time.time()}: Second output')
+
+    # env = GenshinEnv()
+    # env.check_color()
+    # env.run_left()
+    # env.run_left()
+    # env.run_left()
+    # env.run_left()
+    # env.run_left()
--- playground/playground_victory_detection.py
+++ playground/playground_victory_detection.py
@@ -1,19 +1,23 @@
-LETTER_C_1 = (182, 279)
-LETTER_C_2 = (320, 279)
-LETTER_D = (454, 278)
-WHITE = (255, 255, 255)
-
 import pyautogui
 
-while True:
-    if (pyautogui.pixelMatchesColor(
-        *LETTER_C_1, WHITE) and
-        pyautogui.pixelMatchesColor(
-        *LETTER_C_2, WHITE) and
-        pyautogui.pixelMatchesColor(
-        *LETTER_D, WHITE)):
+def check_victory():
+    LETTER_C_1 = (182, 279)
+    LETTER_C_2 = (320, 279)
+    LETTER_D = (454, 278)
+    WHITE = (255, 255, 255)
 
-        print('VICTORY DETECTED')
-        break
-    else:
-        print('not detected')
+    while True:
+        if (pyautogui.pixelMatchesColor(
+            *LETTER_C_1, WHITE) and
+            pyautogui.pixelMatchesColor(
+            *LETTER_C_2, WHITE) and
+            pyautogui.pixelMatchesColor(
+            *LETTER_D, WHITE)):
+
+            print('VICTORY DETECTED')
+            break
+        else:
+            print('not detected')
+
+if __name__ == '__main__':
+    check_victory()
