5ac8725a10d58b951a89bbe0c7da642490651dd0
Hubert Rozmarynowski <hrozmarynowski776@gmail.com>
2024-01-06 19:06:20 +0100

    new daemon approach: events instead of queue

--- playground/playground_background_color_checking.py
+++ playground/playground_background_color_checking.py
@@ -2,19 +2,18 @@
 
 import time
 import multiprocessing
-import queue
 
 import pyautogui
 
-def check_color(queue):
+def check_color(event):
     LOC = (50, 50)
     WHITE = (255, 255, 255)
 
     while True:
         time.sleep(0.2)
         if pyautogui.pixelMatchesColor(*LOC, WHITE):
-            print('White detected, putting True to queue')
-            queue.put(True)
+            print('White detected, event is now True')
+            event.set()
             break
         else:
             print('not detected')
@@ -24,20 +23,25 @@ class PixelChecker:
         self.reset()
 
     def reset(self):
-        self.queue = multiprocessing.Queue()
+        self.event = multiprocessing.Event()
 
         self.color_check_process = multiprocessing.Process(
             target=check_color,
-            args=(self.queue,)
+            args=(self.event,)
         )
         self.color_check_process.start()
 
     def step(self):
-        try:
-            if self.queue.get(block=False) is True:
-                print('step: White detected!')
-        except queue.Empty:
-            print('step: white not detected')
+        if self.event.is_set():
+            print("step: white detected!")
+            print('step: resetting in 3s......')
+            time.sleep(3)
+            print('resetting!')
+            self.reset()
+
+        else:
+            print('step: white not detected yet')
+
 
 if __name__ == '__main__':
     pc = PixelChecker()
